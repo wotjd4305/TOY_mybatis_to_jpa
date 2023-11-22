946d060ebe3bd67127e44db83dd7b219dd3230f6
박재성 <wotjd4305@naver.com>
2023-11-22 16:19:30 +0900

    chore : 기존 필요없는 객체 삭제

--- market_JPA/src/main/java/com/potatoes/potential/route/repo/RouteInfoRepository.java
+++
@@ -1,8 +0,0 @@
-package com.potatoes.potential.route.repo;
-
-import com.potatoes.potential.route.domain.RouteInfo;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface RouteInfoRepository extends JpaRepository<RouteInfo, Long> {
-
-}
